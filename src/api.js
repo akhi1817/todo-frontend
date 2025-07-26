528856dae5aa25bdd98baebf606157a8d9114fd9
akhi1817 <akhileshkumbhar2207@gmail.com>
2025-07-26 21:12:08 +0530

    vercel frontend fix

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
+// src/api.js
 const Baseurl = 'https://todo-backend-wheat.vercel.app/api/todos';
 
 const API = {
   GET_TODOS: `${Baseurl}/get-todos`,
   CREATE_TODOS: `${Baseurl}/create-todo`,
-  DELETE_TODO: (id) => `${Baseurl}/delete-todo/${id}`, // ✅ function for dynamic URL
+  DELETE_TODO: (id) => `${Baseurl}/delete-todo/${id}`,
 };
 
 export default API;
